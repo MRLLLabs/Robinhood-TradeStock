8699db8ba3b8415680890461cda03c5bfe59afe0
Nick Holke <nickholke@gmail.com>
2019-11-27 12:00:27 -0800

    Set up package.json and webpack

--- server/server.js
+++ server/server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const bodyParser = require('body-parser');
 
+app.use(bodyParser.json());
+
+app.use(express.static('public'));
 
 app.listen(port, () => console.log('listening on port: '+ port))
