d51986a086dcc414015c0cf710ee5e9cc36ef384
Nick Holke <nickholke@gmail.com>
2019-11-27 17:12:47 -0800

    Connected database and added schemas for Users and Stocks tables

---
+++ server/database.js
@@ -0,0 +1,48 @@
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize('robinhood', 'root', '', {
+    host: 'localhost',
+    dialect: 'mysql'
+});
+
+sequelize.authenticate().then(()=> console.log('db connect'))
+
+const Users = sequelize.define('users', {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    funds: {
+        type: Sequelize.INTEGER
+    },
+    shares: {
+        type: Sequelize.INTEGER
+    }
+}, {
+    timestamps: false
+});
+
+const Stocks = sequelize.define('stocks', {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    ticker: {
+        type: Sequelize.STRING
+    },
+    price: {
+        type: Sequelize.INTEGER
+    }
+}, {
+    timestamps: false
+})
+
+Users.sync();
+Stocks.sync();
+
+module.exports = {
+    Users: Users,
+    Stocks: Stocks
+};
--- server/server.js
+++ server/server.js
@@ -2,9 +2,12 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const bodyParser = require('body-parser');
+const db = require('./database.js');
 
 app.use(bodyParser.json());
 
 app.use(express.static('public'));
 
-app.listen(port, () => console.log('listening on port: '+ port))
+app.listen(port, () => {
+    console.log('listening on port: '+ port);
+})
