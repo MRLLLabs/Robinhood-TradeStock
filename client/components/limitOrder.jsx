376a7e2a78a07bc30630eb520f6727266919f993
Nick Holke <nickholke@gmail.com>
2019-12-04 09:08:51 -0800

    Small syntax fix

--- client/components/limitOrder.jsx
+++ client/components/limitOrder.jsx
@@ -1,10 +1,8 @@
 /* eslint-disable operator-linebreak */
 import React from 'react';
-import styled from 'styled-components';
 import InputWrapper from './styles/inputWrapper/inputWrapper';
 import Span from './styles/Span/span';
 import OptionWrapper from './styles/Option/optionWrapper';
-import Menu from './styles/Menu/wrapper';
 
 
 class LimitOrder extends React.Component {
