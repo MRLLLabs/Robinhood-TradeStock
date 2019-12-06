6195d5a8bf451adc85500e4359506436b4ca6ee2
Nick Holke <nickholke@gmail.com>
2019-12-06 05:38:27 -0800

    Made custom checkbox

--- client/components/checkbox.jsx
+++ client/components/checkbox.jsx
@@ -5,14 +5,61 @@ const CheckWrapper = styled.div`
   width: 220px;
   margin: auto;
   padding: 10px;
-  color: grey;
-  font-size: 13px;
+  display: flex;
+  justify-content: space-between;
 `;
 
-const CheckBox = () => (
-  <CheckWrapper>
-    <input type="checkbox"></input> Allow this order to execute <br></br> during extended hours
-  </CheckWrapper>
-);
+const TextWrapper = styled.div`
+  color: grey;
+  width: 180px;
+`;
+
+const Box = styled.div`
+  border: 1px solid #20cd99;
+  color: #20cd99;
+  height: 16px;
+  width: 16px;
+  text-align: center;
+  cursor: pointer;
+  margin-top: 5px;
+  background: ${(props) => (props.checked ? '#20cd99' : '')}
+  
+`;
+
+const Check = styled.span`
+  color: #1b1a1d;
+`;
+
+
+class CheckBox extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      checked: false,
+    };
+
+    this.toggleChecked = this.toggleChecked.bind(this);
+  }
+
+  toggleChecked() {
+    this.setState({
+      checked: !this.state.checked,
+    });
+  }
+
+  render() {
+    return (
+    <CheckWrapper>
+      <Box checked={this.state.checked} onClick={this.toggleChecked}>
+        <Check>&#10003;</Check>
+      </Box>
+      <TextWrapper>
+        Allow this order to execute <br></br> during extended hours
+      </TextWrapper>
+    </CheckWrapper>
+    );
+  }
+}
 
 export default CheckBox;
